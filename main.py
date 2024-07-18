4aee124679714ba939afd1a4f151fd96b59e190d
Magnus <nharnharyhaw984@gmail.com>
2024-07-18 22:48:36 +0000

    made final changes

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import face_recognition
 import MySQLdb
 import numpy as np
 import pickle
-import utility
 import datetime
+from utility.EncodeGenerator import EncodeGen
+from models import Student, Attendance
 
 # loads encoding file
 print("Loading encoded file...")
 with open("EncodeFile.p", mode='rb') as file:
-    encode_list_known = pickle.load(file)
+    encode_list_known_ids = pickle.load(file)
 
-#print(encode_list_known_ids)
 # unpacking 
-#encode_list_known, ids = encode_list_known_ids
+encode_list_known, ids = encode_list_known_ids
 #print(encode_list_known)
 #print(ids)
 print("Loading completed")
@@ -24,15 +24,6 @@ capture = cv2.VideoCapture(0)
 #cap.set(3, 1268)
 #cap.set(4, 720)
 
-
-if __name__ == '__main__':
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306, user='root',
-            passwd='windowsql',
-            db='class_attendance_system'
-    )
-cur = db.cursor()
 
 while (True):
     success, img = capture.read()
@@ -45,27 +36,30 @@ while (True):
     encode_curr_frame = face_recognition.face_encodings(img_small, curr_frame)
     for encode_face, face_loc in zip(encode_curr_frame, curr_frame):
         print(len(curr_frame))
-        print("i am in the for loop")
+        #print("i am in the for loop")
         #matches = face_recognition.compare_faces(encode_list_known, encode_face)
         face_distance = face_recognition.face_distance(encode_list_known, encode_face)
+        print("face_distance: ", face_distance)
         min_index = np.argmin(face_distance)
+        print("min_index: ", min_index)
         """The assumption here is that the index is always id - 1"""
-        student_id = min_index + 1
+        student_id = ids[min_index]
         print(f"Student with id {student_id} is present")
-        insert_stmt1 = (
-                "INSERT INTO attendance (date, time, student_id) "
-                "VALUES (%s, %s, %s)"
-                )
-        data = (datetime.date.today(), datetime.datetime.now().time(), student_id)
-        cur.execute(insert_stmt1, data)
-        db.commit()
+        attendance = Attendance(
+            date=datetime.date.today(), time=datetime.datetime.now(), student_id=student_id
+            )
+        EncodeGen.session.add(attendance)
+        EncodeGen.session.commit()
         #print("matches:", matches)
         #print("Distances:", face_distance)
     
 
     cv2.imshow("Attendance", img)
     cv2.waitKey(1)
-    break
 
+
+EncodeGen.session.close()
+"""
 cur.close()
 db.close()
+"""
